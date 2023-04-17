af61b9eae56faa9f4426795db637ac207083bc09
Oleksandra Tiankina <ag7756862@gmail.com>
2023-04-17 03:11:05 +0300

    fix: add if

--- task03.1_greedy/app.js
+++ task03.1_greedy/app.js
@@ -25,10 +25,17 @@ const readInput = async () => {
     const data = await fs.readFile(inputPath, 'utf-8');
     const { weight } = JSON.parse(data);
     console.log(`Current record: ${weight}`);
-    const disks = findCombinationDisks(weight);
-    const newRecord = disks.reduce((acc, curr) => acc + curr, 0) * 2 + GRIP_WEIGHT;
-    console.log(`Optimal disk combination one side: ${disks}`);
-    await log({ prevRecord: weight, combinationX2: disks, newRecord });
+    const result = findCombinationDisks(weight);
+    
+    if (typeof result === 'string') {
+      console.log(result);
+      await log(result);
+
+    } else {
+      const newRecord = result.reduce((acc, curr) => acc + curr, 0) * 2 + GRIP_WEIGHT;
+      console.log(`Optimal disk combination one side: ${result}`);
+      await log({newRecord,singleDiskx2: result, barbellWeight: GRIP_WEIGHT, prevRecord: weight });
+    }
   } catch (err) {
     console.error(`Failed to read the file ${inputPath}`);
     console.error(err);
@@ -36,24 +43,36 @@ const readInput = async () => {
 };
 
 const findCombinationDisks = (recordWeight) => {
-  let optimalDisks = [];
-  let currentWeight = 0;
-  const weightLbsToKg = lbstokg(DISK_WEIGHTS_LBS);
+  if (recordWeight < GRIP_WEIGHT) {
+    console.log(`Weight limit should be greater than grip weight.`);
+    return ` No need for additional disks. The weight of the barbell alone (${GRIP_WEIGHT} kg) is enough to beat the previous record of ${recordWeight} kg.`;
+  }
 
+  const weightLbsToKg = lbstokg(DISK_WEIGHTS_LBS);
   const sortedDisks = DISK_WEIGHTS
     .concat(weightLbsToKg)
     .sort((a, b) => b - a);
-  
+  const maxWeight = sortedDisks
+    .reduce((acc, curr) => acc + curr, 0) * 2 + GRIP_WEIGHT;
+  if (recordWeight >= maxWeight) {
+    console.log(`Weight limit: ${Math.floor(maxWeight)} kg. Insufficient disks.`);
+    return `Weight limit: ${Math.floor(maxWeight)} kg. Insufficient disks.`;
+  }
+
+  const optimalDisks = [];
+  const freeDisks = [];
   const limitWeightOneSide = (recordWeight - GRIP_WEIGHT) / 2;
+  let currentWeight = 0;
+
   for (let i = 0; i < sortedDisks.length; i++) {
-    const disk = sortedDisks[i];
-    if (currentWeight + disk <= limitWeightOneSide) {
+  const disk = sortedDisks[i];
+    if (disk < recordWeight && currentWeight + disk <= limitWeightOneSide) {
       optimalDisks.push(disk);
       currentWeight += disk;
     }
-    continue
+    else freeDisks.push(disk);
   }
-  optimalDisks.push(sortedDisks[sortedDisks.length - 1])
+  optimalDisks.push(freeDisks[freeDisks.length-1])
   return optimalDisks;
 };
 
